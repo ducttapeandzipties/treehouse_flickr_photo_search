24855dd8a7cf4492d54c629e4f5cc49e91492345
Dan Wagner <dan.nmi.wagner@gmail.com>
2016-09-30 19:20:50 -0400

    completed basic app functionality

--- main.js
+++ main.js
@@ -6,15 +6,20 @@ var flickrOptions = {
 };
 
 $('form').submit(function (evt) {
-  evt.preventDefault();
-  var searchTerm = $('#search').val();
-  console.log(searchTerm);
-  flickrOptions.tags = searchTerm;
+  evt.preventDefault();                         //prevents browser from leaving page
+  var searchTerm = $('#search').val();          //captures search term from form
 
-  $.getJSON(flickrAPI, flickrOptions, displayPhotos);
+  flickrOptions.tags = searchTerm;              //sets the search term in the flickrOptions object
+
+  $.getJSON(flickrAPI, flickrOptions, displayPhotos);   //gets data from flickr
 
 });
 
-function displayPhotos(data) {
-  console.log(data);
-}
+function displayPhotos(data) {                    //iterates over returned data and displays it on the page
+  var readout = '<ul>';
+  $.each( data.items, function (i, photo){
+    readout += '<li><img src="' + photo.media.m + '"></li>';
+  });
+  readout += '</ul>';
+  $('#display').html(readout);                  //places html into #display div
+};                                              // end displayPhotos function
